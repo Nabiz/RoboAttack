dbd9690cb23fc10195f27c91e2bff3fc9a893bac
Nabiz <dawcio.gwizdz@gmail.com>
2020-11-13 04:46:21 +0100

    Add Player animation

--- Shooter2D/Player/Player.gd
+++ Shooter2D/Player/Player.gd
@@ -6,13 +6,25 @@ var speed = 200
 var velocity = Vector2.ZERO
 var health = 50
 
+export (Texture) var blaster_texture
+export (Texture) var shotgun_texture
+export (Texture) var rifle_texture
+
 func _ready():
 	emit_signal("damage_taken", health)
 	$Weapon.update_HUD()
+	change_corps_texture("blaster")
 
 func _physics_process(delta):
 	get_movement_input()
 	velocity = move_and_slide(velocity)
+	if velocity.length() > 0:
+		calculate_leg_animation()
+		$LegAnimatedSprite.play()
+	else:
+		$LegAnimatedSprite.set_animation("walk")
+		$LegAnimatedSprite.frame = 0
+		$LegAnimatedSprite.stop()
 
 func get_movement_input():
 	look_at(get_global_mouse_position())
@@ -26,6 +38,47 @@ func get_movement_input():
 	if Input.is_action_pressed("ui_right"):
 		velocity += Vector2(1, 0)
 	velocity = speed * velocity.normalized()
+
+func calculate_leg_animation():
+	var angle = fmod(rotation - velocity.angle() + 2*PI, 2 * PI)
+	if angle < PI/8 or 15*PI/8 <= angle:
+		$LegAnimatedSprite.flip_h = false
+		$LegAnimatedSprite.play("walk")
+	elif PI/8 <= angle and angle < 3*PI/8:
+		$LegAnimatedSprite.flip_h = true
+		$LegAnimatedSprite.play("diagonal_walk")
+	elif 3*PI/8 <= angle and angle < 5*PI/8:
+		$LegAnimatedSprite.flip_h = true
+		$LegAnimatedSprite.play("side_walk")
+	elif 5*PI/8 <= angle and angle < 7*PI/8:
+		$LegAnimatedSprite.flip_h = false
+		$LegAnimatedSprite.play("diagonal_walk_backward")
+	elif 7*PI/8 <= angle and angle < 9*PI/8:
+		$LegAnimatedSprite.flip_h = false
+		$LegAnimatedSprite.play("walk_backward")
+	elif 9*PI/8 <= angle and angle < 11*PI/8:
+		$LegAnimatedSprite.flip_h = true
+		$LegAnimatedSprite.play("diagonal_walk_backward")
+	elif 11*PI/8 <= angle and angle < 13*PI/8:
+		$LegAnimatedSprite.flip_h = false
+		$LegAnimatedSprite.play("side_walk")
+	else:
+		$LegAnimatedSprite.flip_h = false
+		$LegAnimatedSprite.play("diagonal_walk")
+
+func change_corps_texture(wepon_type):
+	if wepon_type == "blaster":
+		$CorpsSprite.texture = blaster_texture
+		$Weapon.position.x = 75
+		$Weapon.position.y = 0
+	elif wepon_type == "shotgun":
+		$CorpsSprite.texture = shotgun_texture
+		$Weapon.position.x = 50
+		$Weapon.position.y = 0
+	elif wepon_type == "rifle":
+		$CorpsSprite.texture = rifle_texture
+		$Weapon.position.x = 75
+		$Weapon.position.y = 10
 
 signal damage_taken(health)
 
--- Shooter2D/Player/Weapon/Weapon.gd
+++ Shooter2D/Player/Weapon/Weapon.gd
@@ -5,9 +5,11 @@ export (PackedScene) var Blaster
 export (PackedScene) var Shotgun
 export (PackedScene) var Rifle
 export (PackedScene) var Granade
+
 var blaster
 var shotgun
 var rifle
+var granade_count = 3
 
 var current_weapon
 
@@ -30,16 +32,21 @@ func _process(delta):
 func change_weapon():
 	if Input.is_action_just_released("ui_blaster"):
 		current_weapon = blaster
+		get_parent().change_corps_texture("blaster")
 		update_HUD()
 	if Input.is_action_just_released("ui_shotgun"):
 		current_weapon = shotgun
+		get_parent().change_corps_texture("shotgun")
 		update_HUD()
 	if Input.is_action_just_released("ui_rifle"):
 		current_weapon = rifle
+		get_parent().change_corps_texture("rifle")
 		update_HUD()
 
 func process_granade():
-	if Input.is_action_just_released("ui_granade"):
+	if Input.is_action_just_released("ui_granade") and granade_count > 0:
+		granade_count -= 1
+		update_granade_HUD()
 		var granade = Granade.instance()
 		granade.position = global_position
 		granade.rotation = get_parent().rotation
@@ -49,6 +56,11 @@ func process_granade():
 func update_HUD():
 	update_ammo_HUD()
 	update_weapon_texture_HUD()
+
+signal update_granade_HUD(granade_count)
+
+func update_granade_HUD():
+	emit_signal("update_granade_HUD", granade_count)
 
 signal update_ammo_HUD(ammo_text)
 
