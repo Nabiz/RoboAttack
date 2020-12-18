45cc399fdc2f624e6ac0af8def460a5aad801d24
Nabiz <dawcio.gwizdz@gmail.com>
2020-12-18 07:09:08 +0100

    Add rocket

--- Shooter2D/Enemies/Boss/Boss.gd
+++ Shooter2D/Enemies/Boss/Boss.gd
@@ -2,12 +2,44 @@ extends "res://Enemies/RangeRobot/RangeRobot.gd"
 
 export (PackedScene) var Explosion
 export (PackedScene) var BossHUD
+export (PackedScene) var Rocket
 
 var is_bossHUD = false
 var bossHUD = null
+var rocket_cooldown = false
 
 func _ready():
 	health = 10000
+
+func calculate_velocity():
+	if engaged:
+		look_at(player.get_global_position())
+		var collider = $RayCast.get_collider()
+		if collider:
+			if collider.name == "Player":
+				if not cooldown:
+					shot()
+					$CooldownTimer.start()
+					cooldown = true
+				if not rocket_cooldown:
+					rocket_shot()
+					$RocketCooldownTimer.start()
+					rocket_cooldown = true
+				if player.position.distance_to(position) <= 600:
+					velocity = Vector2.ZERO
+					$AnimatedSprite.stop()
+					$AnimatedSprite.frame = 0
+				else:
+					velocity = Vector2(speed, 0).rotated(rotation)
+					$AnimatedSprite.play()
+					if $WalkAudioStreamPlayer2D.playing == false:
+						$WalkAudioStreamPlayer2D.play()
+			else:
+				velocity = Vector2.ZERO
+				$AnimatedSprite.stop()
+				$AnimatedSprite.frame = 0
+	if knockback:
+		velocity = -Vector2(speed, 0).rotated(rotation)
 
 func shot():
 	var audio_stream = ShotAudioStream.instance()
@@ -27,6 +59,13 @@ func shot():
 	bullet.rotation = rotation - PI/8
 	bullet.position = $WeaponPosition.global_position
 	get_tree().root.add_child(bullet)
+
+func rocket_shot():
+	$RocketAudioStreamPlayer2D.play()
+	var bullet = Rocket.instance()
+	bullet.rotation = rotation
+	bullet.position = $RocketPosition.global_position
+	get_tree().root.add_child(bullet)	
 
 signal update_boss_HUD(health)
 
@@ -49,3 +88,7 @@ func take_damage(dmg):
 		modulate = Color(1, 0, 0)
 		yield(get_tree().create_timer(0.1), "timeout")
 		modulate = Color(1, 1, 1)
+
+
+func _on_RocketCooldownTimer_timeout():
+	rocket_cooldown = false
